f7c02f11d3ca3ee3970b0b785b084530b9435b88
Alexander Victor <alxv.oliveira@gmail.com>
2020-02-23 10:25:28 -0300

    DELETE

--- projeto/Models/Atendimentos.js
+++ projeto/Models/Atendimentos.js
@@ -37,7 +37,7 @@ class Atendimento {
                 if(erro) {
                     res.status(400).json(erro);
                 } else {
-                    res.status(201).json(resultados);
+                    res.status(201).json({atendimento});
                 }
             });
         }
@@ -80,10 +80,21 @@ class Atendimento {
             if(erro) {
                 res.status(400).json(erro);
             } else {
-                res.status(200).json(resultados);
+                res.status(200).json({...valores, id});
+            }
+        });
+    }
+    
+    deleta(id, res) {
+        const sql = 'DELETE FROM atendimentos WHERE id=?';
+
+        conexao.query(sql, id, (erro, resultados) => {
+            if(erro) {
+                res.status(400).json(erro);
+            } else {
+                res.status(200).json({id});
             }
         });
     }
 }
-
 module.exports = new Atendimento;
